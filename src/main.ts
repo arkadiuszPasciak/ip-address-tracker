ea1a3319f80d01f3cc84c5823ab523773aec4f5a
Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>
2022-01-13 06:34:40 +0100

    IAT-0006: add scss files and config scss files

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+require("@/assets/styles/general/_reset.scss");
+require("@/assets/styles/general/_base.scss");
+
 createApp(App).use(store).mount("#app");
