b7c0a39d892a39c1006f9484d0b4fdee0ce7d1dc
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-01-13 06:45:26 +0100

    IAT-0009: add fonts (#4)
    Co-authored-by: Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,8 @@ module.exports = {
           @import '~@/assets/styles/utils/_media.scss';
           @import '~@/assets/styles/utils/_hover.scss';
           @import '~@/assets/styles/variables/_colors.scss';
+          @import '~@/assets/styles/variables/_fonts.scss';
+          @import '~@/assets/styles/general/_fonts.scss';
         `,
       },
     },
