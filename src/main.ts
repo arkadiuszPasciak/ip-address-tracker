b6f77227d54a18776835fda859f40b115f93d9df
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-01-14 06:35:16 +0100

    IAT-0010: change eslint configs (#6)
    Co-authored-by: Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store'
 
-require("@/assets/styles/general/_reset.scss");
-require("@/assets/styles/general/_base.scss");
+require('@/assets/styles/general/_reset.scss')
+require('@/assets/styles/general/_base.scss')
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(store).mount('#app')
