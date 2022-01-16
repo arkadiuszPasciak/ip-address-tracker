cd08f4d310f5e635c7a8c600d13f8a73fbc3c168
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-01-16 12:22:24 +0100

    IAT-0013: add ui button (#10)
    Co-authored-by: Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>

---
+++ src/components/UI/UIButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button class="UIButton" :class="modifier" type="button" :aria-label="name">
+    <slot name="icon-left"></slot>
+    <slot name="text"></slot>
+    <slot name="icon-right"></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'UIButton',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    modifier: {
+      type: String,
+      default: 'primary',
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.UIButton {
+  padding: 5px;
+
+  &.primary {
+    background-color: $cl-black;
+    color: $cl-white;
+
+    @include hover(hover) {
+      transition: background-color 0.3s ease-in-out;
+    }
+
+    &:hover {
+      @include hover(hover) {
+        background-color: $cl-gray-light;
+      }
+    }
+  }
+}
+</style>
