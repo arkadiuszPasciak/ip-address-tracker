6ac1ad756e6113e90e3d1404ab21f34e82637d6b
Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>
2022-01-16 13:11:13 +0100

    IAT-0020: add component GoogleMap

---
+++ src/components/GoogleMap/GoogleMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="GoogleMap" ref="map"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'GoogleMap',
+  setup() {
+    const root = ref(null)
+
+    return {
+      root,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.GoogleMap {
+  width: 100%;
+  height: 100%;
+}
+</style>
