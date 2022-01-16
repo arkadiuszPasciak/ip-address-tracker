85baf6df0d236bafa3d41772b08a434cb2803f41
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-01-16 12:26:53 +0100

    IAT-0014: add component UIIcon (#11)
    Co-authored-by: Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>

---
+++ src/components/UI/UIIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <i class="UIIcon" :class="name"></i>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'UIIcon',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.UIIcon {
+  display: block;
+  margin: auto;
+
+  &.arrow {
+    width: 10px;
+    height: 14px;
+    mask: url('~@/assets/icons/svg/arrow.svg');
+    background-color: $cl-white;
+  }
+
+  &.location {
+    width: 46px;
+    height: 57px;
+    mask: url('~@/assets/icons/svg/location.svg');
+    background-color: $cl-gray-dark;
+  }
+}
+</style>
