4f5cb576c81b9554834c7c7b3da2f3a086456509
Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>
2022-01-16 12:32:26 +0100

    IAT-0015: add component UIInput

---
+++ src/components/UI/UIInput.vue
@@ -0,0 +1,71 @@
+<template>
+  <input
+    class="UIInput"
+    :class="modifier"
+    :name="name"
+    type="text"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'UIInput',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    modifier: {
+      type: String,
+      default: 'primary',
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.UIInput {
+  padding: 12px;
+  letter-spacing: 0.8px;
+
+  &::placeholder {
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  &:focus {
+    &::placeholder {
+      opacity: 0;
+    }
+  }
+
+  @include media('<=tablet') {
+    &,
+    &::placeholder {
+      font: $font-medium 13px/20px $font-rubik;
+    }
+  }
+
+  @include media('>=tablet') {
+    &,
+    &::placeholder {
+      font: $font-medium 17px/21px $font-rubik;
+    }
+  }
+
+  &.primary {
+    background-color: $cl-white;
+
+    &,
+    &::placeholder {
+      color: $cl-gray-light;
+    }
+  }
+}
+</style>
