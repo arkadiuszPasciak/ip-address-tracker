9d0a16618538ebf054946049732bc3e167bf2981
Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>
2022-01-16 12:47:13 +0100

    IAT-0017: add component TrackerSearch

---
+++ src/components/Tracker/TrackerSearch.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="TrackerSearch">
+    <UIInput
+      class="tracker-input"
+      name="search-ip"
+      placeholder="'TrackerSearch.input.placeholder'"
+    />
+    <UIButton class="tracker-button" name="TrackerSearch.button.name">
+      <template v-slot:icon-right>
+        <UIIcon name="arrow" />
+      </template>
+    </UIButton>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import UIButton from '@/components/UI/UIButton.vue'
+import UIIcon from '@/components/UI/UIIcon.vue'
+import UIInput from '@/components/UI/UIInput.vue'
+
+export default defineComponent({
+  name: 'Tracker',
+  components: {
+    UIButton,
+    UIIcon,
+    UIInput,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.TrackerSearch {
+  display: flex;
+  align-items: center;
+  height: 50px;
+  width: 100%;
+  margin: auto;
+  overflow: hidden;
+  border-radius: 13px;
+
+  @include media('<=tablet') {
+    max-width: 350px;
+  }
+
+  @include media('>=tablet') {
+    max-width: 525px;
+  }
+
+  .tracker-input {
+    width: 100%;
+
+    @include media('<=tablet') {
+      max-width: 300px;
+    }
+
+    @include media('>=tablet') {
+      max-width: 450px;
+    }
+  }
+
+  .tracker-button {
+    width: 100%;
+
+    @include media('<=tablet') {
+      max-width: 50px;
+    }
+
+    @include media('>=tablet') {
+      max-width: 75px;
+    }
+  }
+
+  .tracker-input,
+  .tracker-button {
+    height: 100%;
+  }
+}
+</style>
