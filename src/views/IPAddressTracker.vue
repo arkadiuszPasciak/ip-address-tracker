38d6695bbada83fbfa04f2f8007fe8c53fd5a6f7
Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>
2022-01-16 12:39:41 +0100

    IAT-0016: add component Tracker

--- src/views/IPAddressTracker.vue
+++ src/views/IPAddressTracker.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="IPAddressTracker"></div>
+  <div class="IPAddressTracker">
+    <Tracker />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Tracker from '@/components/Tracker/Tracker.vue'
 
 export default defineComponent({
   name: 'IPAddressTracker',
+  components: {
+    Tracker,
+  },
 })
 </script>
