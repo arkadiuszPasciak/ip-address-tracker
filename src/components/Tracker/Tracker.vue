06c88c0dc837ee06da299a0b9df4d859278d6e07
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-01-16 12:41:03 +0100

    IAT-0016: add component Tracker (#13)
    Co-authored-by: Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>

---
+++ src/components/Tracker/Tracker.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="Tracker">
+    <h1 class="title">Tracker title</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Tracker',
+})
+</script>
+
+<style lang="scss" scoped>
+.Tracker {
+  background: linear-gradient(0.8turn, $cl-red-pastel, $cl-purple-light);
+
+  @include media('<=tablet') {
+    padding: 10px;
+  }
+
+  @include media('>=tablet') {
+    position: relative;
+    height: 100vh;
+    padding: 25px;
+  }
+
+  .title {
+    color: $cl-white;
+    text-align: center;
+
+    @include media('<=tablet') {
+      margin-bottom: 15px;
+      font: $font-bold 24px/28px $font-rubik;
+    }
+
+    @include media('>=tablet') {
+      margin-bottom: 30px;
+      font: $font-bold 32px/36px $font-rubik;
+    }
+  }
+}
+</style>
