bc88f11f88ae0c1634b4990a12b3de159b631715
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-02-05 12:15:57 +0100

    IAT-0035: fix styles (#31)
    Co-authored-by: Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>

--- src/components/UI/UILoader.vue
+++ src/components/UI/UILoader.vue
@@ -38,10 +38,10 @@ export default defineComponent({
     width: 80px;
     height: 80px;
     margin: 8px;
-    border: 8px solid $cl-purple-light;
+    border: 8px solid $cl-black;
     border-radius: 50%;
     animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    border-color: $cl-purple-light transparent transparent transparent;
+    border-color: $cl-black transparent transparent transparent;
     animation-delay: -0.45s;
   }
 }
