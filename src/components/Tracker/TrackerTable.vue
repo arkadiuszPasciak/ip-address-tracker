d505f5e4f4c836ecdba5d2d3805133cc308738a8
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-03-05 12:24:43 +0100

    Iat 0053 fixes after code review (#49)
    * IAT-0053: add description to param
    
    * IAT-0053: add missing elements of interface
    
    * IAT-0054: fix conditions
    
    * IAT-0054: seperate mixins
    
    Co-authored-by: Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>

--- src/components/Tracker/TrackerTable.vue
+++ src/components/Tracker/TrackerTable.vue
@@ -54,11 +54,11 @@ export default defineComponent({
       state.value.isError = false
       state.value.isLoading = true
 
-      if (value.state.ipAddressData !== null) {
+      if (value.state.ipAddressData) {
         state.value.address = value.state.ipAddressData
       }
 
-      if (value.state.isError === true) {
+      if (value.state.isError) {
         state.value.isError = true
       }
 
@@ -73,25 +73,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-@mixin line($position: 'vertical') {
-  content: '';
-  position: absolute;
-  background-color: $cl-gray-light;
-
-  @if $position == 'vertical' {
-    top: 25px;
-    left: 50%;
-    width: 1px;
-    height: calc(100% - 50px);
-  }
-
-  @if $position == 'horizontal' {
-    top: 50%;
-    left: 25px;
-    width: calc(100% - 50px);
-    height: 1px;
-  }
-}
+@import '@/assets/styles/utils/_mixins.scss';
 
 .TrackerTable {
   display: grid;
